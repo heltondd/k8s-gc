38796e2c0e5b62d54fe97b5d1ec2cf3fba3537db
Helton Damascena Dias <heltondd@gmail.com>
2018-10-09 20:46:33 +0000

    change script

--- docker-gc.sh
+++ docker-gc.sh
@@ -2,7 +2,7 @@
 
 while true; do
   # Remove all unused images UNTIL default 24h
-  docker image prune -a -f --filter "until=$UNTIL"
+  docker image prune -a --filter "until=$UNTIL" -f
   # Sleep default INTERVAL 24h
   sleep $INTERVAL
 done
