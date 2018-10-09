91857872160efd0cc5f59e41abba2aab6b2700c5
Helton Damascena Dias <heltondd@gmail.com>
2018-10-09 17:41:40 -0300

    Update docker-gc.sh

--- docker-gc.sh
+++ docker-gc.sh
@@ -3,6 +3,6 @@
 while true; do
   # Remove all unused images UNTIL default 24h
   docker image prune -a -f --filter "until=$UNTIL"
-  # Sleep defualt INTERVAL 24h
+  # Sleep default INTERVAL 24h
   sleep $INTERVAL
 done
